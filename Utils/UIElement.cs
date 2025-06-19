2bafb37d2f4859f0b8b6c8c314438857339110ff
hithere-at <yurifadiila@gmail.com>
2025-06-19 20:55:51 +0700

    Add a semi-working search function

---
+++ Utils/UIElement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace Shadler.UI
+{
+    public static class ShadlerUIElement
+    {
+        public static Button CreateContentButton(string title, string year, string url)
+        {
+            // main items
+            // content banner
+            Image contentImage = new Image
+            {
+                Source = new BitmapImage(new Uri(url)),
+                Width = 150,
+                Height = 240,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+
+            // content title
+            TextBlock contentTitle = new TextBlock
+            {
+                Text = title,
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+
+            // content release year
+            TextBlock contentYear = new TextBlock
+            {
+                Text = year,
+                FontSize = 13,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Opacity = 0.6
+            };
+
+            StackPanel baseLayout = new StackPanel
+            {
+                Orientation = Orientation.Vertical
+            };
+
+            baseLayout.Children.Add(contentImage);
+            baseLayout.Children.Add(contentTitle);
+            baseLayout.Children.Add(contentYear);
+
+            Button button = new Button
+            {
+                Content = baseLayout,
+                Margin = new Thickness(0,0,12,0)
+            };
+
+            return button;
+        }
+    }
+}
--- Views/Browser.xaml.cs
+++ Views/Browser.xaml.cs
@@ -1,3 +1,4 @@
+using ABI.System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,11 +6,19 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.VisualBasic;
+using Shadler.Utils;
+using Shadler.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes; 
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -23,9 +32,114 @@ namespace Shadler.Views
     /// </summary>
     public sealed partial class Browser : Page
     {
+        string contentType = "Anime";
         public Browser()
         {
             this.InitializeComponent();
+        }
+
+        private async void Search_Query(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            Console.WriteLine("Search_Query called");
+            string query = sender.Text;
+            
+            if (string.IsNullOrEmpty(query))
+            {
+                ContentGrid.Children.Clear();
+                return;
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0");
+                client.DefaultRequestHeaders.Add("Referer", "https://allmanga.to");
+                string url;
+
+                if (contentType == "Anime")
+                {
+                    url = Shadler.Utils.Anime.GetQueryUrl(query);
+                }
+                else
+                {
+                    url = Shadler.Utils.Manga.GetQueryUrl(query);
+                }
+
+                Console.WriteLine($"Requesting URL: {url}");
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    using (JsonDocument doc = JsonDocument.Parse(responseData))
+                    {
+                        string what = contentType == "Anime" ? "shows" : "mangas";
+                        JsonElement root = doc.RootElement;
+                        JsonElement contentResults = root.GetProperty("data").GetProperty(what).GetProperty("edges");
+                        int count = 0;
+
+                        ContentGrid.Children.Clear();
+
+                        foreach (JsonElement content in contentResults.EnumerateArray())
+                        {
+                            string thumbnailPath = content.GetProperty("thumbnail").GetString();
+                            thumbnailPath = !(thumbnailPath.StartsWith("https://")) ? "https://aln.youtube-anime.com/" + thumbnailPath : thumbnailPath;
+                            string title = content.GetProperty("name").GetString();
+                            JsonElement date = content.GetProperty("airedStart");   
+                            string year;
+
+                            if (date.TryGetProperty("year", out JsonElement yearElement))
+                            {
+                                year = yearElement.ToString();
+                            } else
+                            {
+                                year = " ";
+                            }
+
+                            Button contentButton = ShadlerUIElement.CreateContentButton(title, year, thumbnailPath);
+                            ContentGrid.Children.Add(contentButton);
+
+                            count++;
+                        }
+                    }
+
+                }
+                else
+                {
+                    ContentGrid.Children.Add(new TextBlock
+                    {
+                        Text = "Uh oh. Something has gone really wrong. Please try again.",
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        FontSize = 16
+                    });
+                }
+            }
+        }
+
+        private void ContentMenu_Click(object sender, RoutedEventArgs args)
+        {
+            ContentGrid.Children.Clear();
+
+            if (sender is MenuFlyoutItem item)
+            {
+                switch (item.Text)
+                {
+                    case "Anime":
+                        contentType = "Anime";
+                        ContentTypeDropDown.Content = "Anime";
+                        break;
+
+                    case "Manga":
+                        contentType = "Manga";
+                        ContentTypeDropDown.Content = "Manga"; 
+                        break;
+                }
+            }
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
         }
     }
 }
